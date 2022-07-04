b4d09129f6c2927577517d4706104909664197a7
Roy Leong <l3ongroy@gmail.com>
2022-07-05 00:29:24 +0800

    add basic boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+const persistor = persistStore(store);
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </PersistGate>
+    </ReduxProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="outlined">Hello World</Button>
+    </main>
+  );
+}
