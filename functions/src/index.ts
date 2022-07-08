5b06a776670c1b6eac1569905bece4aeed4d2992
Roy Leong <l3ongroy@gmail.com>
2022-07-08 12:46:15 +0800

    remove functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,14 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  // functions.logger.info("Hello logs!", {structuredData: true});
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   // functions.logger.info("Hello logs!", {structuredData: true});
 
-  response.send("Hello from Firebase!");
-});
+//   response.send("Hello from Firebase!");
+// });
 
-export const hihi = functions.https.onRequest((req, res) => {
-  res.send("hi thereddee");
-});
+// export const hihi = functions.https.onRequest((req, res) => {
+//   res.send("hi thereddee");
+// });
